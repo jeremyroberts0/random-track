73d4330c6517a65c36009dac0112e745e9344efe
Jeremy Roberts <jeremyroberts0@gmail.com>
2016-03-17 22:18:42 -0700

    Made linking from form work in really lame way

--- app/components/artist-search.js
+++ app/components/artist-search.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    changeArtist(artist) {
-      // TODO: handle artist change
-      var newUrl = '/artist/' + encodeURIComponent(artist);
-      console.error(newUrl);
-    },
     submitArtistForm() {
       var artist = $(this.element).find('input[name="arist"]').val();
-      this.actions.changeArtist(artist);
+      var newUrl = '/artist/' + encodeURIComponent(artist);
+      var controller = this.get('controller');
+      document.location.href = newUrl; // This is really lame but out of time and can't figure out from the docs how to do this right
     }
   }
 });
